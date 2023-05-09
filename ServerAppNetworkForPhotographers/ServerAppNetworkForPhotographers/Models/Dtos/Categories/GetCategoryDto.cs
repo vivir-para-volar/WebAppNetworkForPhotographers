bb2db02ab06b9bc702b88fd5d7849c3fe07a7470
Irene <lyam.irina@yandex.ru>
2023-05-09 18:54:17 +0300

    category: fix

--- ServerAppNetworkForPhotographers/ServerAppNetworkForPhotographers/Models/Dtos/Categories/GetCategoryDto.cs
+++ ServerAppNetworkForPhotographers/ServerAppNetworkForPhotographers/Models/Dtos/Categories/GetCategoryDto.cs
@@ -4,12 +4,14 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int CategoryDirId { get; set; }
         public CategoryDir CategoryDir { get; set; }
 
         public GetCategoryDto(int id, string name, CategoryDir categoryDir)
         {
             Id = id;
             Name = name;
+            CategoryDirId = categoryDir.Id;
             CategoryDir = categoryDir;
         }
     }
