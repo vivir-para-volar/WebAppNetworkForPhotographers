b44ebbac65edb8366a35cc00abb58bbf0dd7c9b2
Irene <lyam.irina@yandex.ru>
2023-06-05 10:09:04 +0300

    Employee Client init

---
+++ EmployeeClientAppNetworkForPhotographers/EmployeeClientAppNetworkForPhotographers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeeClientAppNetworkForPhotographers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
